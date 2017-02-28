4d7839ec3490a801ee857f18730bf4a57b5d7bf9
Patrick Weaver <pjpwea@gmail.com>
2017-02-28 18:25:52 -0500

    switch to json instead of array for network list

---
+++ data/networks.js
@@ -0,0 +1,90 @@
+var networkList = [
+  {
+    "name": "Facebook",
+    "imageURL": "facebook.png",
+    "color": "#3b5998",
+    "statusUpdates": [
+      "Hello World! This is my first post on Facebook!"
+    ]
+  },
+  {
+    "name": "Instagram",
+    "imageURL": "instagram.png",
+    "color": "#bc2a8d",
+    "statusUpdates": [
+      "Hello World! This is my first post on Instagram!"
+    ]
+  },
+  {
+    "name": "LinkedIn",
+    "imageURL": "linkedin.jpeg",
+    "color": "#007bb5",
+    "statusUpdates": [
+      "Hello World! This is my first post on LinkedIn!"
+    ]
+  },
+  {
+    "name": "Musical.ly",
+    "imageURL": "musical.ly.png",
+    "color": "#ff0f50",
+    "statusUpdates": [
+      "Hello World! This is my first post on Musical.ly!"
+    ]
+  },
+  {
+    "name": "Periscope",
+    "imageURL": "periscope.jpeg",
+    "color": "#D95343",
+    "statusUpdates": [
+      "Hello World! This is my first post on Periscope!"
+    ]
+  },
+  {
+    "name": "Snapchat",
+    "imageURL": "snapchat.png",
+    "color": "#fffc00",
+    "statusUpdates": [
+      "Hello World! This is my first post on Snapchat!"
+    ]
+  },
+  {
+    "name": "SoundCloud",
+    "imageURL": "soundcloud.png",
+    "color": "#f50",
+    "statusUpdates": [
+      "Hello World! This is my first post on SoundCloud!"
+    ]
+  },
+  {
+    "name": "Tumblr",
+    "imageURL": "tumblr.png",
+    "color": "#32506d",
+    "statusUpdates": [
+      "Hello World! This is my first post on Tumblr"
+    ]
+  },
+  {
+    "name": "Twitter",
+    "imageURL": "twitter.jpeg",
+    "color": "#55acee",
+    "statusUpdates": [
+      "Hello World! This is my first post on Twitter!"
+    ]
+  },
+  {
+    "name": "WhatsApp",
+    "imageURL": "whatsapp.png",
+    "color": "#4bc959",
+    "statusUpdates": [
+      "Hello World! This is my first post on WhatsApp!"
+    ]
+  },
+  {
+    "name": "YouTube",
+    "imageURL": "youtube.png",
+    "color": "#bb0000",
+    "statusUpdates": [
+      "Hello World! This is my first post on YouTube"
+    ]
+  }
+]
